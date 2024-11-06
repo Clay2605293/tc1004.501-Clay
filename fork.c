539f6c36b32f16228e81b9a1cd69b810af89c226
Clay2605293 <a01220835@tec.mx>
2024-11-06 08:05:25 -0600

    fork

--- fork.c
+++ fork.c
@@ -7,9 +7,11 @@ int main(){
     if (pid==0) {
         a = 10;
         printf("Soy el proceso hijo; variable a = %d\n",a);
+        sleep(5);
+        return 0;
     }
     
     printf("Soy el proceso padre; variable a = %d\n",a);
-
+    sleep(5);
     return 0;
 }
