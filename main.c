ca1efeb3e55873f405d02635bae4df0f1bb40e56
Clay2605293 <153925795+Clay2605293@users.noreply.github.com>
2024-11-13 08:23:37 -0600

    exit

--- main.c
+++ main.c
@@ -3,6 +3,8 @@
 #include <stdlib.h>
 #include <unistd.h>
 
+volatile sig_atomic_t stop = 0;  // Variable para controlar el ciclo
+
 void no_me_matas(int signum) {
     printf("Recibi la señal %d\n", signum);
     printf("No me puedes matar\n");
@@ -11,17 +13,18 @@ void no_me_matas(int signum) {
 void me_matas(int signum) {
     printf("Recibi la señal %d\n", signum);
     printf("Me mataron\n");
-    exit(0);
+    stop = 1;  // Cambia el valor para detener el ciclo
 }
 
 int main() {
-    signal(2, no_me_matas);
-    signal(10, me_matas);
+    signal(2, no_me_matas);  // Signal 2 (SIGINT)
+    signal(10, me_matas);    // Signal 10 (SIGUSR1)
 
-    while (1) {
+    while (stop==0) {  // Revisa la variable `stop` en cada iteración
         printf("Trabajando...\n");
         sleep(1);
     }
 
+    printf("Fin del programa.\n");
     return 0;
 }
